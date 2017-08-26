bb7a5544e392572dcd4361398b5ef7a870b0c9e8
David de los Santos Boix <ddlsb@hotmail.com>
2017-08-26 18:20:30 +0200

    Refactoring

---
+++ scrape.js
@@ -0,0 +1,105 @@
+var { load } = require('cheerio');
+var { each } = require('lodash');
+var { urlArenaVision, urlGuide, selectors, prop, r } = require('./params');
+
+// Obtener los canales y sus URLs de ArenaVision
+exports.getChannels = function(){
+  var channelList = [];
+
+  return new Promise(function (resolve, reject) {
+    r.get(urlArenaVision, async function(error, response, html){
+        if(!error){
+          var $ = load(html);
+          var channels = $(selectors.channels);
+
+          for(i=0;i<channels.length;i++){
+            var channel = await getArenaVisionLink(i+1, urlArenaVision + channels[i].attribs.href);
+            channelList.push(channel);
+          }
+
+          resolve(channelList);
+        } else {
+          reject(error);
+        }
+    });
+  });
+}
+
+// Por el canal, obtener el link
+function getArenaVisionLink(number, url){
+  return new Promise(function (resolve, reject) {
+    r.get(url, function(error, response, html){
+      if(!error){
+        var $ = load(html);
+        var link = $(selectors.acestream);
+        resolve({[number]: link[0].attribs.href});
+      } else {
+        reject(error);
+        console.log();
+      }
+    });
+  });
+}
+
+// Obtener la guía con los canales
+exports.getGuide = function (){
+  return new Promise(function (resolve, reject) {
+    r.get(urlGuide, async function(error, response, html){
+      if(!error){
+        var $ = load(html);
+        var events = $(selectors.events).closest("tr");
+        var eventsInfo = [];
+
+        each(events, function(event) {
+          var info = $(event).find("td");
+          eventsInfo.push(getData(info));
+        })
+        resolve(eventsInfo);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+function getData(info){
+  data = {};
+  data.day = cleanData(info[prop.day]);
+  data.time = cleanData(info[prop.time]);
+  data.sport = cleanData(info[prop.sport]);
+  data.competition = cleanData(info[prop.competition]);
+  data.event = cleanData(info[prop.event]);
+  data.channels = cleanChannels(info[prop.channels]);
+
+  return data;
+}
+
+function cleanData(data){
+  var text = "";
+
+  each(data.children, function(innerText) {
+    if(innerText.data){
+      text += " " + innerText.data.replace("\n", "");
+    }
+  })
+
+  return text.replace("-", " - ").trim();
+}
+
+function cleanChannels(dataChannel){
+  var channels = {};
+
+  each(dataChannel.children, function(text) {
+    if(text.data && text.data.trim() != ""){
+      var rip = text.data.split("[");
+      var lang = rip[1].replace("]", "");
+      var channelsRip = rip[0].trim().split("-")
+
+      each(channelsRip, function(channel){
+        channels[channel] = lang;
+      });
+    }
+  })
+
+  return channels;
+}
